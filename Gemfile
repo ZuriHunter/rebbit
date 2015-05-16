ca35b7c15c8deee9fef2ec53b8432cf642dc9775
Zuri Hunter <zurihunter92@gmail.com>
2015-05-16 05:50:55 +0000

    SetUp and Implement the Voting Gem

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise', '~> 3.3.0'
 gem 'bootstrap-sass', '~> 3.2.0.2'
+gem 'acts_as_votable', '~> 0.10.0'
 
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -82,4 +82,17 @@ class LinksController < ApplicationController
     redirect_to links_path, notice: "Not authorized to access this link" if @link.nil?
   end
   
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_to :back
+  end
+  
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_by current_user
+    redirect_to :back
+  end
+  
+  
 end
